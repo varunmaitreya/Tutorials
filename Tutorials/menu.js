6b7cec335c7ef06c082d6f9897b0f35597ab7f00
Florian Pieper <fpieper@mail.upb.de>
2017-07-30 16:31:03 +0200

    Added global CSS definition and JavaScript for menu handling

---
+++ Tutorials/menu.js
@@ -0,0 +1,11 @@
+function openMenu(){
+	document.getElementById("MainMenu").style.width = "75%";
+	//document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+	return 0;
+}
+
+function closeMenu(){
+	document.getElementById("MainMenu").style.width = "0%";
+	document.body.style.backgroundColor = "rgba(255,255,255,255)";
+	return 0;
+}
